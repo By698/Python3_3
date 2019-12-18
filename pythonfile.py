693c6979988bb3ae784a06adb73fbca242ce1c45
By698 <bysiek1998@gmail.com>
2019-12-18 21:23:00 +0100

    push data into lists

--- pythonfile.py
+++ pythonfile.py
@@ -1,1 +1,10 @@
+import numpy as np
 
+data = np.loadtxt('iris.data')
+#
+# 1. sepal length in cm
+#    2. sepal width in cm
+#    3. petal length in cm
+#    4. petal width in cm
+#    5. class:
+sepal_length, sepal_width, petal_length, petal_width, iris_class = data.T
