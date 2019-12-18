fca2c0ef454bfc978a9257e3b653a8f14e83ac21
By698 <bysiek1998@gmail.com>
2019-12-18 22:39:29 +0100

    cechy jakościowe

--- pythonfile.py
+++ pythonfile.py
@@ -1,16 +1,16 @@
 import numpy as np
-
+from scipy.stats import mode
 values = np.loadtxt('iris.data', delimiter=',', usecols=[0, 1, 2, 3])
 labels = np.loadtxt('iris.data', dtype='U15', delimiter=',', usecols=[4])
-#    1. sepal length in cm
-#    2. sepal width in cm
-#    3. petal length in cm
-#    4. petal width in cm
-#    5. class
-
 
 sepal_length, sepal_width, petal_length, petal_width = values.T
 iris_class = labels.T
-avg = np.average(sepal_length)
 
-print(avg)
+
+def dominant(data):
+    _ = mode(data)
+
+    print('Najczęściej występująca wartość to:', _[0][0])
+    print('Liczebność:', _[1][0])
+    print('Częstość:', _[1][0] / len(data))
+
