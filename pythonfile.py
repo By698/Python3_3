4a486983125c6df8027ea3f05289f6dd715191d2
By698 <bysiek1998@gmail.com>
2019-12-18 21:51:10 +0100

    push data into lists

--- pythonfile.py
+++ pythonfile.py
@@ -1,10 +1,16 @@
 import numpy as np
 
-data = np.loadtxt('iris.data')
-#
-# 1. sepal length in cm
+values = np.loadtxt('iris.data', delimiter=',', usecols=[0, 1, 2, 3])
+labels = np.loadtxt('iris.data', dtype='U15', delimiter=',', usecols=[4])
+#    1. sepal length in cm
 #    2. sepal width in cm
 #    3. petal length in cm
 #    4. petal width in cm
-#    5. class:
-sepal_length, sepal_width, petal_length, petal_width, iris_class = data.T
+#    5. class
+
+
+sepal_length, sepal_width, petal_length, petal_width = values.T
+iris_class = labels.T
+avg = np.average(sepal_length)
+
+print(avg)
