7ca6967350e4ccd99959de670f6b23549f20ff63
Phanty <kamil_chmielecki@wp.pl>
2019-12-18 22:38:56 +0100

    Dane ilosciowe

--- pythonfile.py
+++ pythonfile.py
@@ -1,10 +1,42 @@
 import numpy as np
 
-data = np.loadtxt('iris.data')
-#
-# 1. sepal length in cm
+values = np.loadtxt('iris.data', delimiter=',', usecols=[0, 1, 2, 3])
+labels = np.loadtxt('iris.data', dtype='U15', delimiter=',', usecols=[4])
+#    1. sepal length in cm
 #    2. sepal width in cm
 #    3. petal length in cm
 #    4. petal width in cm
-#    5. class:
-sepal_length, sepal_width, petal_length, petal_width, iris_class = data.T
+#    5. class
+
+
+sepal_length, sepal_width, petal_length, petal_width = values.T
+iris_class = labels.T
+
+
+def srednia_arytmetyczna(dana):
+    avg = np.average(dana)
+
+    return avg
+
+
+def odchylenie_standardowe(dana):
+    std = np.std(sepal_length)
+
+    return std
+
+def mediana(dana):
+    median = np.median(dana)
+
+    return median
+
+
+def minimum(dana):
+    min = np.minimum(dana)
+
+    return min
+
+
+def maximum(dana):
+    max = np.maximum(dana)
+
+    return max
