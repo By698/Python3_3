63f2ba91a54b194882f972dc1f66e810e06567b6
By698 <bysiek1998@gmail.com>
2019-12-19 13:22:54 +0100

    histogramy i mapa cieplna

--- pythonfile.py
+++ pythonfile.py
@@ -35,26 +35,42 @@ def dane_ilosciowe(dana, name):
 def correlation_matrix(data):
     print(np.corrcoef(data))
 
+
 def linear_regression(dana1,dana2):
-
-
-    X = dana1.reshape(-1, 1)
+    x = dana1.reshape(-1, 1)
     y = dana2
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0)
 
     regr = linear_model.LinearRegression()
 
-    regr.fit(X_train,y_train)
+    regr.fit(x_train, y_train)
 
-    y_pred = regr.predict(X_test)
+    y_pred = regr.predict(x_test)
 
-    plt.scatter(X_test, y_test, color='black')
-    plt.plot(X_test, y_pred, color='blue', linewidth=3)
+    plt.scatter(x_test, y_test, color='black')
+    plt.plot(x_test, y_pred, color='blue', linewidth=3)
     plt.show()
 
 
-correlation_matrix(values.T)
+def histograms(x, y, x_name, y_name):
+    fig, axs = plt.subplots(1, 2, constrained_layout=True)
+    fig.patch.set_facecolor('white')
+    fig.suptitle("Histogram dla dwóch najbardziej ze sobą skorelowanych cech ilościowych")
+    axs[0].hist(x)
+    axs[0].set_title(x_name)
+    axs[1].hist(y)
+    axs[1].set_title(y_name)
+    plt.show()
 
-dane_ilosciowe(sepal_length, 'sepal_length')
 
-linear_regression(petal_length,petal_width)
+def mapa_cieplna(x, y):
+    fig, ax = plt.subplots(tight_layout=True)
+    hist = ax.hist2d(x, y)
+    plt.show()
+
+
+# correlation_matrix(values.T)
+# histograms(petal_width, petal_length, 'petal_width', 'petal_length')
+# mapa_cieplna(petal_width, petal_length)
+# dane_ilosciowe(sepal_length, 'sepal_length')
+# linear_regression(petal_length,petal_width)
