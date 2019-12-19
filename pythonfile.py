53dc5bdb68dd177576b175d2b17e0985ca1ecf3d
Phanty <kamil_chmielecki@wp.pl>
2019-12-19 13:00:33 +0100

    Krzywa regresji v1.0

--- pythonfile.py
+++ pythonfile.py
@@ -1,5 +1,8 @@
 import numpy as np
 from scipy.stats import mode
+import matplotlib.pyplot as plt
+from sklearn import datasets, linear_model
+from sklearn.model_selection import train_test_split
 values = np.loadtxt('iris.data', delimiter=',', usecols=[0, 1, 2, 3])
 labels = np.loadtxt('iris.data', dtype='U15', delimiter=',', usecols=[4])
 
@@ -32,7 +35,26 @@ def dane_ilosciowe(dana, name):
 def correlation_matrix(data):
     print(np.corrcoef(data))
 
+def linear_regression(dana1,dana2):
+
+
+    X = dana1.reshape(-1, 1)
+    y = dana2
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+    regr = linear_model.LinearRegression()
+
+    regr.fit(X_train,y_train)
+
+    y_pred = regr.predict(X_test)
+
+    plt.scatter(X_test, y_test, color='black')
+    plt.plot(X_test, y_pred, color='blue', linewidth=3)
+    plt.show()
+
 
 correlation_matrix(values.T)
 
 dane_ilosciowe(sepal_length, 'sepal_length')
+
+linear_regression(petal_length,petal_width)
